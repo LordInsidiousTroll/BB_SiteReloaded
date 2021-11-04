8b36877c9c6d734f377795bd61572d20555a0ec7
LordInsidiousTroll <lord.insidioustroll@gmail.com>
2021-11-04 02:30:23 -0500

    Mild cleanup.

--- BB_SiteReloaded/Scripts/Bingo.js
+++ BB_SiteReloaded/Scripts/Bingo.js
@@ -1,4 +1,8 @@
-﻿$(document).ready(function () {
+﻿/*
+ * This is a script that changes the color of a cell when it gets clicked on.
+ * If you're reading this and you're not performing site maintenance, then wtf are you doing with your life? You should also tell Lord your answer to this question.
+ */
+$(document).ready(function () {
 
 	$(".fkup").click(function () {
 		$cell = $(this);
